6a18905a78740b1e7481fff3ae0e2cf25c11d71d
Mahesh3232 <mahesh.aher1999@gmail.com>
2022-12-11 22:28:16 +0530

    Added

---
+++ cypress/integration/Actions/Actions.spec.js
@@ -0,0 +1,37 @@
+
+
+
+//MouseAction
+describe('Validation of mouse actions',()=>{
+
+    it('Verify the functionality of scrollintoview',()=>{
+        cy.visit('http://www.webdriveruniversity.com/')
+        cy.get('#datepicker').scrollIntoView().click()
+    })
+
+    it('Verify the functionality of DragAndDrop',()=>{
+        cy.visit('http://www.webdriveruniversity.com/')
+        cy.get('#actions').invoke('removeAttr','target').wait(1000).click()
+        cy.get('#draggable').trigger('mousedown',{which:1})
+        cy.get('#droppable').trigger('mousemove')
+        .trigger('mouseup',{force:true})
+        cy.get('#droppable >p >b').should('have.text','Dropped!')
+    })
+
+    it('Verify the functionality of DoubleClick',()=>{
+        cy.visit('http://www.webdriveruniversity.com/')
+        cy.get('#actions').invoke('removeAttr','target').wait(1000).click()
+        cy.get('#double-click').dblclick()
+        cy.get('#double-click').should('have.class','div-double-click double')
+    })
+
+    it.only('Verify the functionality of mouseDown',()=>{
+        cy.visit('http://www.webdriveruniversity.com/')
+        cy.get('#actions').invoke('removeAttr','target').wait(1000).click()
+        cy.wait(2000)
+        cy.get('#click-box').trigger('mousedown',{which:1})
+        cy.get('#click-box').should('have.text','Well done! keep holding that click now.....')
+        cy.get('#click-box').trigger('mouseup')
+        cy.get('#click-box').should('have.text','Dont release me!!!')
+    })
+})
--- cypress/integration/Assignment/Assign.spec.js
+++ cypress/integration/Assignment/Assign.spec.js
@@ -1,24 +1,27 @@
 describe('Verify the login', () => {
     let SecondUrl =
-    it('Verify thelogin', () => {
-        cy.on('uncaught:exception', (err, runnable) => {
-            return false
-        });
-        cy.visit('https://www.trip.com/')
-        cy.wait(2000)
-        cy.get('#credentials-picker >div >div').click({force:true})
-        // cy.get('.mc-hd__login-btn').click()
-        // cy.window().then(win => {
-        //     cy.stub(win, 'open').callsFake((url, target) => {
-        //         //cy.log(url)
-        //         SecondUrl = url
-        //         return win.open.wrappedMethod.call(win, url, '_self')
-        //     }).as('open');
-        //     cy.get('.way_icon_gl').click()
-        //     cy.wait(3000)
-        //    // cy.log(SecondUrl)
-        //     //  cy.get('@open'); 
-        //     cy.origin(SecondUrl)
-        // });
-    })
+        it('Verify thelogin', () => {
+            cy.on('uncaught:exception', (err, runnable) => {
+                return false
+            });
+            cy.visit('https://www.trip.com/')
+            cy.wait(2000)
+            cy.get('#credentials-picker >div >div').click({ force: true })
+            // cy.get('.mc-hd__login-btn').click()
+            // cy.window().then(win => {
+            //     cy.stub(win, 'open').callsFake((url, target) => {
+            //         //cy.log(url)
+            //         SecondUrl = url
+            //         return win.open.wrappedMethod.call(win, url, '_self')
+            //     }).as('open');
+            //     cy.get('.way_icon_gl').click()
+            //     cy.wait(3000)
+            //    // cy.log(SecondUrl)
+            //     //  cy.get('@open'); 
+            //     cy.origin(SecondUrl)
+            // });
+        })
 })
+
+
+
